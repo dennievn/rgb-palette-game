31e0a147cf54612705558a56224f7ff04e81c035
duongvn <ddvnguyen92@gmail.com>
2018-04-21 13:46:08 -0600

    add how-to-play alert

--- script.js
+++ script.js
@@ -12,6 +12,8 @@ var h1 = document.querySelector("h1");
 var resetButton = document.querySelector("#reset");
 var modeButtons = document.querySelectorAll(".mode");
 
+
+
 init();
 
 function init(){
@@ -98,3 +100,4 @@ function randomColor(){
     var b = Math.floor(Math.random() * 256);
     return "rgb(" + r + ", " + g + ", " + b + ")";
 }
+alert("WHAT IS RGB: The rgb(red, green, blue) Color Model is used to represent the colors on a electronic display. Each color uses a combination of decimals numbers ranging from 0 to 255 to form a wide array of colors. Example: Blue = rgb(0, 0, 255) because there are no reds and greens. Purple = rgb(128, 0, 128) because we all know that purple is a combination of red and blue mixed. HOW TO PLAY: The screen will display a rgb number and it is your job to select the circle that matches the right color code. The backdrop will change color if you guessed it right and the uncorrect circle will fadeout if you guessed it wrong. You have an option of selecting Easy Mode which will give you only 3 circles to choose from, and you are also welcome to shuffle a new color anytime if you are stuck. HAVE FUN!");
